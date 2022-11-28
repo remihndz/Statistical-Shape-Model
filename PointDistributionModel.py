4c2d98f82662d28ae68ce50842e01fe755d470e7
Remi Hernandez <remi.hernandez@liverpool.ac.uk>
2022-11-28 09:38:12 +0000

    Point distribution model for a vessel rather than a pair of vessels.

--- PointDistributionModel.py
+++ PointDistributionModel.py
@@ -27,7 +27,7 @@ class PointDistributionModel:
         number of shapes in the training set
     nFeatures : int
         number of features in each shape
-
+    
     Methods:
     --------
     fit()
@@ -36,7 +36,10 @@ class PointDistributionModel:
         set the data attribute
     generate(int n)
         returns a list of n new shapes (arrays of size (nFeatures/2, 2))    
-    GeneralizedProcrustesAnalysis(list Shapes)    
+    GeneralizedProcrustesAnalysis(list Shapes)
+        returns the data in the shape space
+    to_cco(str filename)
+        save the shape into a cco file
     """
 
     def __init__(self, Shapes):
@@ -55,7 +58,7 @@ class PointDistributionModel:
         
         
     
-    def setTrainingSet(self, Shapes):
+    def setTrainingSet(self, Shapes : list):
         self._data = np.zeros((self.nFeatures, self.nSamples))
         newShapes, self._meanShape = self.GeneralizedProcrustesAnalysis(Shapes, maxIter=5)
         for i, shape in enumerate(newShapes):
@@ -70,7 +73,7 @@ class PointDistributionModel:
         self._stds = np.sqrt(np.abs(eigva))
         self._isFitted = True
 
-    def generate(self, n, stdFactor=5):
+    def generate(self, n : int, stdFactor=5) -> list:
         if not self._isFitted:
             raise ValueError("The model has not been trained yet.")
         newShapes = []
@@ -82,7 +85,7 @@ class PointDistributionModel:
         
 
     @staticmethod
-    def GeneralizedProcrustesAnalysis(Shapes, error = 1e-1, maxIter = 10):
+    def GeneralizedProcrustesAnalysis(Shapes:list, error = 1e-1, maxIter = 10) -> list:
         # Shapes is a list of k shapes made of (L, 2) L 2D landmarks
         refShape = Shapes[0] # Arbitrary selected
         k = len(Shapes)
@@ -108,3 +111,80 @@ class PointDistributionModel:
                 break
         newShapes[0] = refShape
         return newShapes, meanShape.reshape((-1,))
+
+    
+
+    
+    # @staticmethod
+    # def to_cco(filename, CRA : point, vessels : list, vesselsType = 'DEFORMABLE_PARENT'):
+    #     """
+    #     Write vessels in cco format filename.
+
+    #     Arguments:
+    #     ----------
+    #     filename: the name of the file to save the tree
+    #     CRA: the location of the CRA, used as root for each vessel in vessels
+    #     vessels: a list of list or lists of numpy arrays containing the points (excluding CRA) forming a vessel     
+    #     """
+                    
+    #     ## TODO use a proper length and radius for the CRA 
+    #     # Use the length of a vessel segment as an estimate of the CRA's length (because unit is unknown)
+
+    #     if isinstance(vessels[0], float):
+    #         vesselsList = [vessels]
+    #     elif isinstance(vessels, list):
+    #         vesselsList = vessels
+    #     else:
+    #         raise ValueError("Please provide a valide list of arrays or a single array.")
+        
+    #     lengthFirstVessel = np.linalg.norm(np.array(vesselsList[0][1])-np.array(vesselsList[0][0]))*3
+    #     xperf  = np.array(CRA)-np.array([0.0,0.0,lengthFirstVessel])
+    #     qProx  = 2.5e-4 # muL/min? Taken from the results of the DCCO algorithm
+    #     psiFr  = 9.6e-6 # psiFactor
+    #     dp     = 50*133.3 # Pa
+    #     nTerms = len(vesselsList)
+
+    #     refPressure = 18*133.3 # Pa
+    #     rootRadius  = 7e-3    # in cm = 70microns
+    #     variationTol = 1e-6
+
+    #     branchingTypeDir = {'NO_BRANCHING':0,
+    #                         'RIGID_PARENT':1,
+    #                         'DEFORMABLE_PARENT':2,
+    #                         'DISTAL_BRANCHING':3,
+    #                         'ONLY_AT_PARENT_HOTSPOTS':4}
+    #     branchingType = branchingTypeDir[vesselsType]
+
+
+    #     nSegments = 1 # The CRA
+    #     for vessel in vessels:
+    #         nSegments += np.array(vesselsList).size
+
+    #     nSegment += len(vesselsList)
+            
+    #     with open(filename, 'w') as f:
+
+    #         f.write("*Tree\n")
+    #         f.write(f"{xperf[0]} {xperf[1]} {xperf[2]} {qProx} {psiFr} {dp} {nTerms} {refPressure} 1 {rootRadius} {variationTol}\n")
+
+    #         f.write("\n*Vessels\n")
+    #         f.write(f"{nSegments}\n")
+
+    #         vesselId = 0
+    #         stage    = 0
+    #         vesselConnectivity = dict()
+    #         vesselConnectivity[0] = [-1]
+            
+            
+    #         f.write(f"0 {xperf[0]} {xperf[1]} {xperf[2]} {CRA[0]} {CRA[1]} {CRA[2]} 0.0 0.0 0.0 0.0 3 {rootRadius} {qProx} 0.0 0.0 0.0 0 0.0 0.0 {stage}")
+    #         stage+=1
+    #         vesselId+=1
+            
+    #         for vessel in vesselsList:
+    #             xProx, parent = np.array(CRA), 0
+    #             vessel = np.array(vessel).ravel().reshape((-1,2))
+    #             for i in range(np.array(vessel).ravel().size):
+    #                 distalPoint = vessel[i, :]
+    #                 vesselConnectivity[vesselId] = [parent]
+                
+        
